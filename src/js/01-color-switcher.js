4490707d319a1a3f117e941b8d4eef3880e20752
Lesandra17 <Lesya.lisa1708@gmail.com>
2023-11-03 18:54:04 +0200

    summary

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+    }
+
+    let intervalId;
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+
+    startButton.addEventListener('click', () => {
+      if (intervalId) return;
+
+      startButton.disabled = true;
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    });
+     stopButton.addEventListener('click', () => {
+      clearInterval(intervalId);
+      startButton.disabled = false;
+      intervalId = null;
+    });
 
